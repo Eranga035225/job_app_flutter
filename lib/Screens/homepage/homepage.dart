bbc13b5b1e7ad2a75b2f14fb5d8bf5c7a5e76e24
Eranga <erangakavishanka@icloud.com>
2025-12-02 18:02:49 +0530

    created new folder for widgets

--- lib/Screens/homepage/homepage.dart
+++ lib/Screens/homepage/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/app_bar/app_bar.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -37,52 +39,3 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-      Container(
-        width: 50, 
-        height: 50,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(15),
-          border: Border.all(
-            color: Colors.grey.shade400,
-          )
-    
-    
-        ),
-        child: Icon(Icons.menu, color: Colors.grey.shade800,size:28)
-    
-      ),
-      Text('Creative Jobs', style: TextStyle(
-      fontSize: 24,
-      fontWeight: FontWeight.bold
-        ),
-      ),
-    
-      Container(
-        width: 50,
-        height: 50,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(15),
-          border: Border.all(
-            color: Colors.grey.shade400,
-          )
-        ),
-        child: Icon(Icons.person, color: Colors.grey.shade800,size: 28),
-    
-        
-      )
-    
-    ],);
-  }
-}
---
+++ lib/Screens/homepage/widgets/app_bar/app_bar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+      Container(
+        width: 50, 
+        height: 50,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(
+            color: Colors.grey.shade400,
+          )
+    
+    
+        ),
+        child: Icon(Icons.menu, color: Colors.grey.shade800,size:28)
+    
+      ),
+      Text('Creative Jobs', style: TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold
+        ),
+      ),
+    
+      Container(
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(
+            color: Colors.grey.shade400,
+          )
+        ),
+        child: Icon(Icons.person, color: Colors.grey.shade800,size: 28),
+    
+        
+      )
+    
+    ],);
+  }
+}
