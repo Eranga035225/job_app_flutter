455268eb5b265c85707df27e3028135a732787a3
Eranga <erangakavishanka@icloud.com>
2025-12-02 22:22:11 +0530

    developed splash screen

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+
+import 'homepage/homepage.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,10 +13,58 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), (){
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => HomePage(),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.grey.shade200,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('JobMate', style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold
+                ),),
+                SizedBox(width: 10,),
+                Icon(Icons.work_outline_sharp, size: 30,)
+              ],
+            ),
+            Text('Find your dream job', style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey.shade600
+            ),),
+
+            SizedBox(height: 40,),
+
+            CircularProgressIndicator(
+              color: Colors.grey.shade800,
+              strokeWidth: 2,
+              
+
+              
+            )
+
+          ],
+        ),
+      ),
+
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:job_app_flutter/Screens/splash_screen.dart';
 
 import 'Screens/homepage/homepage.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
      
-      home: HomePage(), 
+      home: SplashScreen(), 
     );
   }
 }
