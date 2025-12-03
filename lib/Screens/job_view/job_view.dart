741849dcf3b8df1f3bed8623378e36b81a9a44e8
Eranga <erangakavishanka@icloud.com>
2025-12-03 15:34:12 +0530

    added images

--- lib/Screens/job_view/job_view.dart
+++ lib/Screens/job_view/job_view.dart
@@ -12,7 +12,28 @@ class _JobViewState extends State<JobView> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
-      body: 
+      body: Column(
+        children: [
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage('https://tse1.mm.bing.net/th/id/OIP.1mha44Q-rfM9hrpOvQ0UnwHaFj?rs=1&pid=ImgDetMain&o=7&rm=3'))
+
+              
+
+            ),
+          )
+
+
+        
+      
+        ],
+      
+      
+      
+      )
       
 
 
