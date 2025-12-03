64c5f60055f193e22b0781ee55bbf573639a9dbe
Eranga <erangakavishanka@icloud.com>
2025-12-03 15:42:09 +0530

    developed section 1

--- lib/Screens/job_view/job_view.dart
+++ lib/Screens/job_view/job_view.dart
@@ -10,20 +10,38 @@ class JobView extends StatefulWidget {
 class _JobViewState extends State<JobView> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size; 
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
       body: Column(
         children: [
           Container(
             height: 250,
+            width: size.width,
             decoration: BoxDecoration(
               image: DecorationImage(
                 fit: BoxFit.cover,
                 image: NetworkImage('https://tse1.mm.bing.net/th/id/OIP.1mha44Q-rfM9hrpOvQ0UnwHaFj?rs=1&pid=ImgDetMain&o=7&rm=3'))
 
-              
-
             ),
+            child: Stack(
+              children: [
+                SafeArea(child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: GestureDetector(
+                    onTap: (){
+                      Navigator.pop(context);
+                    },
+                    child: CircleAvatar(
+                      backgroundColor: Colors.black.withAlpha(120),
+                      child: Icon(Icons.arrow_back, color: Colors.white,),
+                    ),
+                  ),
+                )),
+              ],
+              
+            )
+            
           )
 
 
